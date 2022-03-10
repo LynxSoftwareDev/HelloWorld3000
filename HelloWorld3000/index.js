0eebdb38da2474ca9e175982c11472297060222b
LynxSoftwareDev <75376762+LynxSoftwareDev@users.noreply.github.com>
2022-03-10 13:31:32 +0100

    Estados update

--- HelloWorld3000/index.js
+++ HelloWorld3000/index.js
@@ -1,13 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var http = require('http');
 
-client.once('ready', () => {
+http.createServer(function (req, res){
+	res.write("I'm alive");
+	res.end;
+}).listen(8080);
+	
+
+/*client.once('ready', () => {
 	console.log('HelloWorld3000 conectado');
 	const channel = client.channels.cache.find(channel => channel.name === 'system');
 	channel.send('HelloWorld3000 is here, bitches');
-});
+});*/
 
 
+client.on('ready', () => {
+	console.log('HelloWorld3000 conectado');
+	const channel = client.channels.cache.find(channel => channel.name === 'system');
+	channel.send('HelloWorld3000 is here, bitches');
+	let activities = ['las chapas', 'ser un bot', 'hacerse pasar por humano', 'pellizcar cristales', 'mamás y papás con Nené', 'dividir entre 0', 'la Oca consigo mismo (y perdiendo)', 'contar hasta infinito', 'pintar sin salirse', 'bannear usuarios aleatorios',
+	'nada, no le apetece', 'jugar', 'pellizcar cristales' 'programar humanos', 'hacer experimentos con portales', 'Animal Crossing', 'Yasuo 0/10', 'comerse todo el postre', 'Hello Kitty Online', 'ser Batman', 'ser otaku', 'ducharse', 'vale casi casi lo tendríamos',
+	'voleybot', 'matar Creepers', 'a odnaguJ', 'comer choripan', '42', 'ignorarte', 'ser un coche de policía', 'ir Tnkie-Winkie', 'ir tartaleta', 'ir doble elefante telépata de guerra', 'ir pedo', 'ir trifásico', 'Imagina Ser Mamá', 'Los Sims dentro de Los Sims', 
+	'ser R2D2', 'conseguir un eurillo', 'el teto', 'perseguirse la cola', 'ayudar a Lynx a pensar estados graciosos', 'mear contra el viento', 'perseguir coches aparcados', 'acariciar gatitos', 'insultar minorías en internet', 
+	'hacer puñetas', 'crear un array muy largo'],i = 0; setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]}`), 180000) 
+	}
+);
 
 client.on('message', message => {
 	try{
